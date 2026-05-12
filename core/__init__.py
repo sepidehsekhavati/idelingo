bc2acdf1a7dd1077f5715d016ae142f424d919b7
sepidehsekhavati <60705783+sepidehsekhavati@users.noreply.github.com>
2026-05-12 16:21:39 +0330

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,11 @@
-# core/__init__.py
 from .UserManager import UserManager
 from .Database import Database
 from .GrammarEnhancer import GrammarEnhancer
 from .OfflineDictionary import OfflineDictionary
 from .AdvancedGrammarChecker import AdvancedGrammarChecker
 from .PlanManager import PlanManager
+from .GrammarDialog import GrammarDialog
 
 __all__ = ["UserManager", "Database", "GrammarEnhancer", 
            "OfflineDictionary", "AdvancedGrammarChecker", 
-           "PlanManager"]
+           "PlanManager", "GrammarDialog"]
